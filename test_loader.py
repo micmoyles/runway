708c24f90308d9eecf2a1e00a5c9cbf3b57d2e9f
Michael Moyles <mmoyles@raspberrypi>
2016-07-26 14:27:37 +0000

    revert to mysql

--- test_loader.py
+++ test_loader.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
 import base_app
-x = base_app.loader('/mnt/data/REMIT/','postgres')
-x.whitelist = ['SOSO']
+x = base_app.loader('/mnt/data/REMIT/','localhost')
+x.whitelist = ['FREQ','SOSO']
 x.username = 'root'
 x.passwd = 'wiarreft'
 x.cleanup = True 
 x.timeout = 0.2
 x.status = False
-x.sql = 'psql'
+x.sql = 'mysql'
 x.__start__()

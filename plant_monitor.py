fa5367e3d191c4ca148fc0199261ef7081693de1
Michael Moyles <mmoyles@raspberrypi>
2016-08-05 10:13:42 +0000

    method to generate query intervals

--- plant_monitor.py
+++ plant_monitor.py
@@ -12,6 +12,8 @@ class plant_monitor(EApp):
     EApp.__init__( self )
     self.interval = []
     self.plants = []
+    self.monitorLength = 5 # days into the future
+    self.monitorInterval = 30# interval to monitor (minutes)
     self.query = '''
         select messageCreationTs as ts,NormalCapacity,AvailableCapacity from outages 
         where EventStart < '%s' 
@@ -20,21 +22,37 @@ class plant_monitor(EApp):
         order by ts desc 
         limit 1 
 '''
+  def runAssertions( self ):
+    assert len(self.plants) > 0, 'Need to specify at least one plant'
+    assert self.monitorLength is not None
+    assert self.monitorInterval is not None
+
+  def getIntervals( self ):
+    maxMins = int(self.monitorLength * 24 * 60)
+    intervals = range(0, maxMins + self.monitorInterval, self.monitorInterval) 
+    print intervals
+
   def check_status( self, time, plant ):
     time_for_query = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+    time_for_query = (datetime.datetime.now() + datetime.timedelta(minutes=time)).strftime( '%Y-%m-%d %H:%M:%S')
     query = self.query % ( str(time_for_query), str(time_for_query), plant) 
-    print query
     db = mdb.connect( host, user, passwd)
     cursor = db.cursor( mdb.cursors.DictCursor )
     cursor.execute( 'use REMIT' )
     cursor.execute( query )
     row = cursor.fetchone() # fetching one here is fine as we are only expecting one result
-    if row is not None: print plant, row['AvailableCapacity']
+    if row is not None: print plant, time, row['AvailableCapacity']
     cursor.close()
   
   def start( self ):
+    self.runAssertions()
+    self.getIntervals()
     for plant in self.plants:
-      self.check_status( 0, plant )
+      self.check_status(  30, plant )
+      self.check_status(  60, plant )
+      self.check_status(  90, plant )
+      self.check_status( 120, plant )
+
 p = plant_monitor()
-p.plants = ['DRAXX-1','WBUPS-1','CNQPS-3','T_CNQPS-3']
+p.plants = ['DRAXX-1','T_CNQPS-3']
 p.start()

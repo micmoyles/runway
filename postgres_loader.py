86515568f5544eef619461cc24a1e7b33b778395
mmoyles <micmoyles@gmail.com>
2016-10-30 12:53:39 +0000

    add postgres loader

---
+++ postgres_loader.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import base_app
+x = base_app.loader('/data/REMIT','localhost')
+x.whitelist = ['FREQ','SOSO']
+x.username = 'erova'
+x.passwd = '05LHTo0bMQc4kv'
+x.hostname = 'postgres.cfqfdoajjaoa.eu-west-1.rds.amazonaws.com'
+x.cleanup = True 
+x.timeout = 0.2
+x.status = False
+x.sql = 'psql'
+x.__start__()

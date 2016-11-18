773fca606bf86797154310cee3f3571bfddc56b1
mmoyles <micmoyles@gmail.com>
2016-11-18 13:58:20 +0000

    host details

--- mysqlToPostgres.py
+++ mysqlToPostgres.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 
 import MySQLdb as mdb
+username = 'erova'
+hostname = 'localhost'
+passwd = 'er0va123'
 
 
 def get_known_Assets():
     query = '''
    select name, AssetID, FuelType, NormalCapacity from plants
 '''
-    db = mdb.connect( self.db, self.username, self.passwd)
+    db = mdb.connect( hostname, username, passwd)
     cursor = db.cursor( mdb.cursors.DictCursor )
     cursor.execute( 'use config' )
     cursor.execute( query )

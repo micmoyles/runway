e349b86166df5aa659921cff398bb4d9fb274bd9
root <root@ip-172-31-45-132.eu-west-1.compute.internal>
2016-11-18 15:41:46 +0000

    scfipt is working

--- mysqlToPostgres.py
+++ mysqlToPostgres.py
@@ -22,9 +22,10 @@ conn=pdb.connect("  dbname='config' \
                          password='05LHTo0bMQc4kv'")
 
 p_cursor = conn.cursor()
-p_cmd = 'insert into plants (name, assetid, fueltype, normalcapacity) values( '%s', '%s', '%s', %d)'
+p_cmd = 'insert into plants (name, assetid, fueltype, normalcapacity) values( \'%s\', \'%s\', \'%s\', %d);'
 
 for row in rows:
+    print( p_cmd % (row['name'], row['AssetID'], row['FuelType'],row['NormalCapacity']) )
     p_cursor.execute( p_cmd % (row['name'], row['AssetID'], row['FuelType'],row['NormalCapacity']) )
 
 conn.commit()

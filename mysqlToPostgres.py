5e878534e47c135808fc83a9d5e7d36ccd25c737
mmoyles <micmoyles@gmail.com>
2016-11-18 14:04:47 +0000

    add postgres connection

--- mysqlToPostgres.py
+++ mysqlToPostgres.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python
 
 import MySQLdb as mdb
+import psycopg2 as pdb
 username = 'erova'
 hostname = 'localhost'
 passwd = 'er0va123'
 
 
-def get_known_Assets():
-    query = '''
+query = '''
    select name, AssetID, FuelType, NormalCapacity from plants
 '''
-    db = mdb.connect( hostname, username, passwd)
-    cursor = db.cursor( mdb.cursors.DictCursor )
-    cursor.execute( 'use config' )
-    cursor.execute( query )
-    rows = cursor.fetchall()
-    for row in rows:
-        print row
-    cursor.close()
+db = mdb.connect( hostname, username, passwd)
+m_cursor = db.cursor( mdb.cursors.DictCursor )
+m_cursor.execute( 'use config' )
+m_cursor.execute( query )
+rows = m_cursor.fetchall()
 
-get_known_Assets()
+conn=pdb.connect("  dbname='config' \
+                         user='erova' \
+                         host='postgres.cfqfdoajjaoa.eu-west-1.rds.amazonaws.com' \
+                         password='05LHTo0bMQc4kv'")
+
+p_cursor = conn.cursor()
+p_cmd = 'insert into plants (name, assetid, fueltype, normalcapacity) values( %s, %s, %s, %d)'
+
+for row in rows:
+    print p_cmd % (row['name'], row['AssetID'], row['FuelType'],row['NormalCapacity'])
+
+
+m_cursor.close()
+p_cursor.close()
+

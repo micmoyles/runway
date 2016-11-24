6183e6dd794d6bee5dca9205045d9588c9b1d8ce
root <root@ip-172-31-45-132.eu-west-1.compute.internal>
2016-11-24 21:07:24 +0000

    add individual launch scripts for subscriber and loader

---
+++ launch_loader.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/usr/bin/python /home/erova/runway/postgres_loader.py &> loader.log &
+
+
---
+++ launch_subscriber.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/usr/bin/python /home/erova/runway/elexon_push_data.py &> elexon_push_data.log &
+

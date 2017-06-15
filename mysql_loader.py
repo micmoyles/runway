3121e25a0a5c1b09d5ddaace2165c70a3635776d
Michael Moyles <mmoyles@puppet>
2017-06-15 09:25:43 +0000

    define hostname

--- mysql_loader.py
+++ mysql_loader.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
 import loader
-x = loader.loader('/data/REMIT','localhost')
+x = loader.loader('/data/REMIT')
 x.whitelist = ['FREQ','SOSO']
 x.appName = 'LOADER'
 x.username = 'erova'
 x.passwd = 'er0va123'
+x.hostname = 'localhost'
 x.cleanup = True 
 x.timeout = 0.2
 x.status = False

413b114c784153cb6ff72ccc47a59fbd365a95c7
Michael Moyles <micmoyles@gmail.com>
2018-10-31 11:13:22 +0000

    remove credentials from old database

--- postgres_loader.py
+++ postgres_loader.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 
-import loader
+import runway.loader as loader
 x = loader.loader('/data/REMIT')
 x.appName = 'LOADER'
 x.whitelist = []
-x.username = 'erova'
-x.passwd = '05LHTo0bMQc4kv'
+x.username = 'blank'
+x.passwd = 'blank'
 x.hostname = 'postgres.cfqfdoajjaoa.eu-west-1.rds.amazonaws.com'
 x.cleanup = True 
 x.timeout = 0.2
